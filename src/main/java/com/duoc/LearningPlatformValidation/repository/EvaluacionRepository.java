1bb4673ff52db15b9859404d3efad50c2903033c
CRISTIAN JAVIER TORRES MARTINEZ <ctorresm06@gmail.com>
2026-04-30 22:19:55 -0400

    add repository interfaces for Curso, Evaluacion, Inscripcion, and Usuario

---
+++ src/main/java/com/duoc/LearningPlatformValidation/repository/EvaluacionRepository.java
@@ -0,0 +1,7 @@
+package com.duoc.LearningPlatformValidation.repository;
+
+import com.duoc.LearningPlatformValidation.model.Evaluacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EvaluacionRepository extends JpaRepository<Evaluacion, Long> {
+}
---
+++ src/main/java/com/duoc/LearningPlatformValidation/repository/InscripcionRepository.java
@@ -0,0 +1,7 @@
+package com.duoc.LearningPlatformValidation.repository;
+
+import com.duoc.LearningPlatformValidation.model.Inscripcion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InscripcionRepository extends JpaRepository<Inscripcion, Long> {
+}
